a87268a71bc0239f01a8b8ba0c5edb372dd323de
Benjamin Dobler <benjamin.dobler@gmail.com>
2025-10-19 14:36:57 +0200

    feat: integrate @richapps/ui-components and @richapps/ngx-drag, enhance sidebar functionality and styling

--- projects/demo/src/app/components/sidebar/sidebar.component.ts
+++ projects/demo/src/app/components/sidebar/sidebar.component.ts
@@ -3,10 +3,11 @@ import { Settings } from '../../service/settings';
 import { FormsModule } from '@angular/forms';
 import { RecordingService } from '../../service/recording.service';
 import { EditorService } from '../../service/editor.service';
+import { ColorComponent, EdSelectComponent, EdSelectOptionComponent, InputComponent } from '@richapps/ui-components';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [FormsModule],
+  imports: [FormsModule, InputComponent, ColorComponent, EdSelectComponent, EdSelectOptionComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
